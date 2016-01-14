319284f22582cf48c3904e2935416036472b58d9
Jonas Höglund <jonas.hoglund@embeddednation.com>
2016-01-14 15:19:58 +0100

    Added layer2interface to inteno base config

--- iopsys-brcm63xx-mips/dg301/profiles/inteno.mk
+++ iopsys-brcm63xx-mips/dg301/profiles/inteno.mk
@@ -1,7 +1,7 @@
 
 define Profile/inteno
   NAME:=Inteno
-  PACKAGES:=bcmkernel bcmhotproxy brcm-base-files iopsys-base-files natalie-dect natalie-dect-h dectmngr
+  PACKAGES:=bcmkernel bcmhotproxy brcm-base-files iopsys-base-files natalie-dect natalie-dect-h dectmngr layer2interface
   CONFIG=KEN_CONFA
 endef
 
