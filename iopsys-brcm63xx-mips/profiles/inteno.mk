9739d96c6754b9335ee16c559d1a9d7307aa71d8
Jonas Höglund <jonas.hoglund@embeddednation.com>
2016-03-23 17:05:48 +0100

    Set TARGET_MODEL_NAME and TARGET_IBOARDID based on selected profile

---
+++ iopsys-brcm63xx-mips/profiles/inteno.mk
@@ -0,0 +1,22 @@
+
+define Profile/cg300
+	NAME:=cg300
+endef
+
+define Profile/cg300/Description
+	cg300 profile
+endef
+
+$(eval $(call Profile,cg300))
+
+
+define Profile/cg301
+	NAME:=cg301
+endef
+
+define Profile/cg301/Description
+	cg301 profile
+endef
+
+$(eval $(call Profile,cg301))
+
