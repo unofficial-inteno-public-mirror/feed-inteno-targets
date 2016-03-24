d313344ef066f357aae58f48ce1922b13556d01b
Jonas Höglund <jonas.hoglund@embeddednation.com>
2016-03-24 13:47:15 +0100

    Add all iopsys-brcm63xx-mips profiles

--- iopsys-brcm63xx-mips/profiles/inteno.mk
+++ iopsys-brcm63xx-mips/profiles/inteno.mk
@@ -1,4 +1,3 @@
-
 define Profile/cg300
 	NAME:=cg300
 endef
@@ -19,4 +18,137 @@ define Profile/cg301/Description
 endef
 
 $(eval $(call Profile,cg301))
+
+
+define Profile/dg150
+	NAME:=dg150
+endef
+
+define Profile/dg150/Description
+	dg150 profile
+endef
+
+$(eval $(call Profile,dg150))
+
+
+define Profile/dg150alv2
+	NAME:=dg150alv2
+endef
+
+define Profile/dg150alv2/Description
+	dg150alv2 profile
+endef
+
+$(eval $(call Profile,dg150alv2))
+
+
+define Profile/dg150v2
+	NAME:=dg150v2
+endef
+
+define Profile/dg150v2/Description
+	dg150v2 profile
+endef
+
+$(eval $(call Profile,dg150v2))
+
+
+define Profile/dg200
+	NAME:=dg200
+endef
+
+define Profile/dg200/Description
+	dg200 profile
+endef
+
+$(eval $(call Profile,dg200))
+
+
+define Profile/dg200al
+	NAME:=dg200al
+endef
+
+define Profile/dg200al/Description
+	dg200al profile
+endef
+
+$(eval $(call Profile,dg200al))
+
+
+define Profile/dg301
+	NAME:=dg301
+endef
+
+define Profile/dg301/Description
+	dg301 profile
+endef
+
+$(eval $(call Profile,dg301))
+
+
+define Profile/dg301al
+	NAME:=dg301al
+endef
+
+define Profile/dg301al/Description
+	dg301al profile
+endef
+
+$(eval $(call Profile,dg301al))
+
+
+define Profile/dg351
+	NAME:=dg351
+endef
+
+define Profile/dg351/Description
+	dg351 profile
+endef
+
+$(eval $(call Profile,dg351))
+
+
+define Profile/eg200
+	NAME:=eg200
+endef
+
+define Profile/eg200/Description
+	eg200 profile
+endef
+
+$(eval $(call Profile,eg200))
+
+
+define Profile/eg300
+	NAME:=eg300
+endef
+
+define Profile/eg300/Description
+	eg300 profile
+endef
+
+$(eval $(call Profile,eg300))
+
+
+define Profile/vg50
+	NAME:=vg50
+endef
+
+define Profile/vg50/Description
+	vg50 profile
+endef
+
+$(eval $(call Profile,vg50))
+
+
+define Profile/vox25
+	NAME:=vox25
+endef
+
+define Profile/vox25/Description
+	vox25 profile
+endef
+
+$(eval $(call Profile,vox25))
+
 
