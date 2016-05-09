6bcada7903a0f8222572994bd261f1e63cd0d649
Jonas Höglund <jonas.hoglund@embeddednation.com>
2016-05-09 12:52:19 +0200

    Remove conflicting asterisk package for mips

--- iopsys-brcm63xx-mips/profiles/inteno.mk
+++ iopsys-brcm63xx-mips/profiles/inteno.mk
@@ -2,7 +2,7 @@
 ASTERISK_PACKAGES:=asterisk18-mod asterisk18-mod-app-playtones asterisk18-mod-app-read \
 asterisk18-mod-app-system asterisk18-mod-app-transfer \
 asterisk18-mod-chan-brcm asterisk18-mod-codec-a-mu asterisk18-mod-codec-alaw \
-asterisk18-mod-codec-g722 asterisk18-mod-codec-g726 asterisk18-mod-codec-ulaw \
+asterisk18-mod-codec-g722 asterisk18-mod-codec-g726 \
 asterisk18-mod-format-g726 asterisk18-mod-format-g729 asterisk18-mod-format-sln \
 asterisk18-mod-format-sln16 asterisk18-mod-func-channel asterisk18-mod-func-db \
 asterisk18-mod-func-shell asterisk18-mod-pbx-spool asterisk18-mod-res-stun \
