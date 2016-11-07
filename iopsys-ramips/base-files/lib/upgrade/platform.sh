5d7e83cbdb9f01aea05a48d6fb89543df513ca65
Kenneth Johansson <kenneth@southpole.se>
2016-11-07 19:16:52 +0100

    Sysupgrade.
    horrible but it works if using the -n options.

--- iopsys-ramips/base-files/lib/upgrade/platform.sh
+++ iopsys-ramips/base-files/lib/upgrade/platform.sh
@@ -1,264 +1,182 @@
-#
-# Copyright (C) 2010 OpenWrt.org
-#
-
-. /lib/ramips.sh
-
-PART_NAME=firmware
-RAMFS_COPY_DATA=/lib/ramips.sh
-
-platform_check_image() {
-	local board=$(ramips_board_name)
-	local magic="$(get_magic_long "$1")"
-
-	[ "$#" -gt 1 ] && return 1
-
-	case "$board" in
-	3g150b|\
-	3g300m|\
-	3g-6200n|\
-	3g-6200nl|\
-	a5-v11|\
-	ai-br100|\
-	air3gii|\
-	all0239-3g|\
-	all0256n|\
-	all5002|\
-	all5003|\
-	ar725w|\
-	asl26555|\
-	awapn2403|\
-	awm002-evb|\
-	awm003-evb|\
-	bc2|\
-	broadway|\
-	carambola|\
-	cf-wr800n|\
-	cs-qr10|\
-	d105|\
-	dap-1350|\
-	db-wrt01|\
-	dcs-930|\
-	dcs-930l-b1|\
-	dir-300-b1|\
-	dir-300-b7|\
-	dir-320-b1|\
-	dir-600-b1|\
-	dir-600-b2|\
-	dir-615-d|\
-	dir-615-h1|\
-	dir-620-a1|\
-	dir-620-d1|\
-	dir-810l|\
-	duzun-dm06|\
-	e1700|\
-	esr-9753|\
-	ex2700|\
-	f7c027|\
-	firewrt|\
-	fonera20n|\
-	freestation5|\
-	gl-mt300a|\
-	gl-mt300n|\
-	gl-mt750|\
-	hc5*61|\
-	hg255d|\
-	hlk-rm04|\
-	hpm|\
-	ht-tm02|\
-	hw550-3g|\
-	ip2202|\
-	jhr-n805r|\
-	jhr-n825r|\
-	jhr-n926r|\
-	linkits7688|\
-	linkits7688d|\
-	m2m|\
-	m3|\
-	m4|\
-	microwrt|\
-	miniembplug|\
-	miniembwifi|\
-	miwifi-mini|\
-	miwifi-nano|\
-	mlw221|\
-	mlwg2|\
-	mofi3500-3gn|\
-	mpr-a1|\
-	mpr-a2|\
-	mr-102n|\
-	mt7628|\
-	mzk-750dhp|\
-	mzk-dp150n|\
-	mzk-ex300np|\
-	mzk-ex750np|\
-	mzk-w300nh2|\
-	mzk-wdpr|\
-	nbg-419n|\
-	nw718|\
-	oy-0001|\
-	pbr-m1|\
-	psg1208|\
-	psr-680w|\
-	px-4885|\
-	re6500|\
-	rp-n53|\
-	rt5350f-olinuxino|\
-	rt5350f-olinuxino-evb|\
-	rt-g32-b1|\
-	rt-n10-plus|\
-	rt-n13u|\
-	rt-n14u|\
-	rt-n15|\
-	rt-n56u|\
-	rut5xx|\
-	sap-g3200u3|\
-	sk-wb8|\
-	sl-r7205|\
-	tew-691gr|\
-	tew-692gr|\
-	tiny-ac|\
-	ur-326n4g|\
-	ur-336un|\
-	v22rw-2x2|\
-	vocore|\
-	w150m|\
-	w306r-v20|\
-	w502u|\
-	wf-2881|\
-	whr-1166d|\
-	whr-300hp2|\
-	whr-600d|\
-	whr-g300n|\
-	widora-neo|\
-	witi|\
-	wizfi630a|\
-	wl-330n|\
-	wl-330n3g|\
-	wl-341v3|\
-	wl-351|\
-	wli-tx4-ag300n|\
-	wmr-300|\
-	wnce2001|\
-	wndr3700v5|\
-	wr512-3gn|\
-	wr6202|\
-	wrh-300cr|\
-	wrtnode|\
-	wrtnode2r |\
-	wrtnode2p |\
-	wsr-600|\
-	wt1520|\
-	wt3020|\
-	wzr-agl300nh|\
-	x5|\
-	x8|\
-	y1|\
-	y1s|\
-	zbt-ape522ii|\
-	zbt-wa05|\
-	zbt-we826|\
-	zbt-wg2626|\
-	zbt-wg3526|\
-	zbt-wr8305rt|\
-	zte-q7|\
-	youku-yk1)
-		[ "$magic" != "27051956" ] && {
-			echo "Invalid image type."
-			return 1
-		}
-		return 0
-		;;
-	ar670w)
-		[ "$magic" != "6d000080" ] && {
-			echo "Invalid image type."
-			return 1
-		}
-		return 0
-		;;
-	br-6475nd)
-		[ "$magic" != "43535953" ] && {
-			echo "Invalid image type."
-			return 1
-		}
-		return 0
-		;;
-	c20i|\
-	c50)
-		[ "$magic" != "03000000" ] && {
-			echo "Invalid image type."
-			return 1
-		}
-		return 0
-		;;
-	cy-swr1100|\
-	dir-610-a1|\
-	dir-645|\
-	dir-860l-b1)
-		[ "$magic" != "5ea3a417" ] && {
-			echo "Invalid image type."
-			return 1
-		}
-		return 0
-		;;
-	wsr-1166)
-		[ "$magic" != "48445230" ] && {
-			echo "Invalid image type."
-			return 1
-		}
-		return 0
-		;;
-	ubnt-erx)
-		nand_do_platform_check "$board" "$1"
-		return $?;
-		;;
-	esac
-
-	echo "Sysupgrade is not yet supported on $board."
-	return 1
+platform_check_image_size() {
+	[ "$(check_image_size "$1")" == "SIZE_OK" ] || {
+		return 1
+	}
+	return 0
 }
 
-platform_nand_pre_upgrade() {
-	local board=$(ramips_board_name)
+platform_check_image() {
+	local from img_type
 
-	case "$board" in
-	ubnt-erx)
-		platform_upgrade_ubnt_erx "$ARGV"
-		;;
+	[ "$ARGC" -gt 1 ] && return 1
+
+	echo "Image platform check started ..." > /dev/console
+
+	case "$1" in
+		http://*|ftp://*) get_image "$1" "cat" > /tmp/firmware.bin; from=/tmp/firmware.bin;;
+		*) from=$1;;
 	esac
+
+	[ "$(check_crc "$from")" == "CRC_OK" ] || {
+		echo "CRC check failed" > /dev/console
+		return 1
+	}
+
+	case "$(get_image_type "$from")" in
+		"INTENO") img_type=2 ;;
+		"CFE+FS") img_type=1 ;;
+		"FS")     img_type=0 ;;
+		*)
+			echo "Unknown image type" > /dev/console
+			return 1
+	esac
+	if [ $img_type -lt 2 ] && grep -q "rootfstype=ubifs" /proc/cmdline; then
+		echo "Old image not compatible after upgrade to UBIFS" > /dev/console
+		return 1
+	fi
+	echo $img_type > /tmp/CFE_FS
+
+	if [ -e /etc/ssl/certs/opkg.pem ]; then
+	    if ! check_sig "$from"; then
+		echo "Signature of file is wrong. Aborting!" > /dev/console
+		return 1
+	    fi
+	fi
+
+	case "$(get_flash_type "$from")" in
+		"NAND")
+			echo 1 > /tmp/IS_NAND
+			;;
+		"NOR")
+			echo 0 > /tmp/IS_NAND
+			;;
+		*)
+			echo "Unknown flash type" > /dev/console
+			return 1
+	esac
+
+	[ "$(check_image_size "$from")" == "SIZE_OK" ] || {
+		echo "Image size is too large" > /dev/console
+		return 1
+	}
+
+	echo "get_image_chip_id $(get_image_chip_id $from)"
+	echo	"get_chip_id $(get_chip_id)"
+	if [ "$(get_image_chip_id "$from")" != "$(get_chip_id)" ]; then
+		echo "Chip model of image does not match" > /dev/console
+		return 1
+	fi
+
+	# Customer name check should be carried out
+	# only if a regarding parameter set in config.
+	# For now skip customer name check.
+	if [ 1 -eq 0 ]; then
+		[ -f /lib/db/version/iop_customer ] \
+			&& [ "$(get_image_customer "$from")" != "$(cat /lib/db/version/iop_customer)" ] && {
+			echo "Image customer doesn't match" > /dev/console
+			return 1
+		}
+		# NOTE: expr interprets $(db get hw.board.hardware) as a
+		# regexp which could give unexpected results if the harware
+		# name contains any magic characters.
+		expr "$(get_image_model_name "$from")" : "$(db get hw.board.hardware)" || {
+			echo "Image model name doesn't match board hardware" > /dev/console
+			return 1
+		}
+	fi
+
+	echo "Image platform check completed" > /dev/console
+
+	return 0
 }
 
 platform_pre_upgrade() {
-	local board=$(ramips_board_name)
 
-	case "$board" in
-    	ubnt-erx)
-		nand_do_upgrade "$ARGV"
-		;;
+	local from mtd_no
+	local cfe_fs
+	local is_nand
+	local klink sname
+
+	cfe_fs=$(cat /tmp/CFE_FS)
+	is_nand=$(cat /tmp/IS_NAND)
+
+	case "$1" in
+		http://*|ftp://*) from=/tmp/firmware.bin;;
+		*) from=$1;;
 	esac
+
+	if [ $is_nand -ne 1 ]; then
+		v "This is not a nand image, aborting!"
+		exit 1
+	fi
+
+	# Stop unnecessary processes
+	for klink in $(ls /etc/rc.d/K[0-9]*); do
+		sname=$(basename $(readlink -f $klink))
+		case $sname in
+			bcmhotproxy|boot|cgroups|dropbear|iwatchdog|telnet|umount)
+				v "Not stopping $sname"
+				;;
+			*)
+				v "Stopping $sname..."
+				$klink stop
+				;;
+		esac
+	done
+	sleep 3s
+
+	#DEBUG
+	#ps > /dev/console
+	#DEBUG
+
+	if [ "$SAVE_OVERLAY" -eq 1 -a -z "$USE_REFRESH" ]; then
+		v "Creating save overlay file marker"
+		touch /SAVE_OVERLAY
+	else
+		v "Not saving overlay files"
+		rm -f /SAVE_OVERLAY
+	fi
+	if [ "$SAVE_CONFIG" -eq 1 -a -z "$USE_REFRESH" ]; then
+		v "Creating save config file marker"
+		touch /SAVE_CONFIG
+	else
+		v "Not saving config files"
+		rm -f /SAVE_CONFIG
+	fi
+	sync
+
+	if [ $cfe_fs -eq 2 ]; then
+	    inteno_image_upgrade $from
+	else
+		# Old/Brcm format image
+		if [ $cfe_fs -eq 1 ]; then
+			v "Writing CFE ..."
+			cfe_image_upgrade $from 0 131072
+		fi
+
+		v "Writing File System ..."
+		mtd_no=$(find_mtd_no "rootfs_update")
+		if [ $cfe_fs -eq 1 ]; then
+			update_sequence_number $from 0 131072
+			imagewrite -c -k 131072 /dev/mtd$mtd_no $from
+		else
+			update_sequence_number $from 0
+			imagewrite -c /dev/mtd$mtd_no $from
+		fi
+	fi
+	sync
+
+#	v "Setting bootline parameter to boot from newly flashed image"
+#	brcm_fw_tool set -u 0
+#	v "Current Software Upgrade Count: $(get_rootfs_sequence_number)"
+
+	v "Upgrade completed!"
+	rm -f $from
+	[ -n "$DELAY" ] && sleep "$DELAY"
+	v "Rebooting system ..."
+	sync
+	export REBOOT_REASON=upgrade
+	echo "KEN !!!!! will now run exit 1"
+	reboot -f
 }
 
-platform_do_upgrade() {
-	local board=$(ramips_board_name)
-
-	case "$board" in
-	*)
-		default_do_upgrade "$ARGV"
-		;;
-	esac
-}
-
-disable_watchdog() {
-	killall watchdog
-	( ps | grep -v 'grep' | grep '/dev/watchdog' ) && {
-		echo 'Could not disable watchdog'
-		return 1
-	}
-}
-
-blink_led() {
-	. /etc/diag.sh; set_state upgrade
-}
-
-append sysupgrade_pre_upgrade disable_watchdog
-append sysupgrade_pre_upgrade blink_led
